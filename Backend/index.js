b1b2525f162481fc050e7821854a6dc8142b71fd
Aryan <chaudharyaryan3456@gmail.com>
2025-02-06 18:44:33 +0530

    udpated

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,50 @@
-const express = require("express");
-const app = express();
-const port = 8080;
+// const express = require("express");
+// const app = express();
+// const port = 8080;
 
-app.use(express.urlencoded({extended : true}));
-app.use(express.json());
+// app.use(express.urlencoded({extended : true}));
+// app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`listening on port ${port}`);
+// });
 
-app.get("/register", (req, res) => {
-    let { user, password } = req.query;
-    res.send(`standard GET Request. Welcome ${user}`);
-});
+// app.get("/register", (req, res) => {
+//     let { user, password } = req.query;
+//     res.send(`standard GET Request. Welcome ${user}`);
+// });
 
-app.post("/register", (req, res) => {
-    let { user, password } = req.body;
-    res.send(`standard POST Request. Welcome ${user}`);
-});
+// app.post("/register", (req, res) => {
+//     let { user, password } = req.body;
+//     res.send(`standard POST Request. Welcome ${user}`);
+// });
+
+class Mammal {      //Parent Class (base class)
+    constructor(name) {
+        this.name = name;
+        this.type = "warm-blooded";
+    }
+    eat() {
+        console.log("I am eating");
+    }
+}
+
+class dog extends Mammal{
+    constructor(name) {
+        super(name);
+    }
+
+    talk() {
+        console.log("waff..");
+    }
+}
+
+class cat extends Mammal{
+    constructor(name) {
+        super(name);
+    }
+
+    talk() {
+        console.log("meow..");
+    }
+}
